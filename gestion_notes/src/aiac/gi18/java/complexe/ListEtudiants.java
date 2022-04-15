3e0cb422eb49f33e31180bd627418349069149eb
simot <simot@192.168.56.1>
2022-04-15 15:48:33 +0000

    1

--- gestion_notes/src/aiac/gi18/java/complexe/ListEtudiants.java
+++ gestion_notes/src/aiac/gi18/java/complexe/ListEtudiants.java
@@ -1,5 +1,13 @@
-package aiac.gi18.java.complexe;
+package gestion_notes;
 
-public class ListEtudiants {
+import java.util.ArrayList;
+
+public class ListEtudiants extends ArrayList<etudiant> {
+	
+	@Override
+	public boolean add(etudiant e) {
+		if(this.contains(e)) return false;
+		return super.add(e);
+	}
 
 }
