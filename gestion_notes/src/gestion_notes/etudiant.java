cc9f69920ad6e2a95fb620c0b57ac911c18155a5
simot <simot@192.168.56.1>
2022-04-15 14:41:15 +0000

    2

--- gestion_notes/src/gestion_notes/etudiant.java
+++ gestion_notes/src/gestion_notes/etudiant.java
@@ -5,5 +5,12 @@ public class etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	
+	public etudiant(int id , String nom) {
+		this.id=id;
+		this.nom=nom;
+		this.note=10;
+		
+	}
 
 }
